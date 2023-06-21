5cf5ad0ffde94914c0b724734cee1c8d80051110
shankuan <shan.kuang@keysight.com>
2023-06-21 11:16:46 +0800

    alloy demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { AlloyCardModule } from '@keysight/alloy';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlloyCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
